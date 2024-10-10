254e8ddfc8ffd6ef87ba1b5f881fd14df4717aee
Wayaer <wayaer@foxmail.com>
2024-10-10 14:33:19 +0800

    no message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.3
+
+* Update version
+
 ## 0.0.2
 
 * Change the `padding` of `PickerOptions` to `titlePadding`
--- lib/fl_list_wheel.dart
+++ lib/fl_list_wheel.dart
@@ -1,5 +1,3 @@
-library fl_list_wheel;
-
 export 'src/fl_list_wheel.dart';
 export 'src/options.dart';
 export 'src/picker/picker.dart';
